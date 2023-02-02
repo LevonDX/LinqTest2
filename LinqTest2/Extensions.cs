da6a1ebaaa5a826378e08c215f0c6adf294fc85d
Levon Hovhannisyan <lehovhan@microsoft.com>
2023-02-02 20:22:50 +0400

    Add project files.

---
+++ LinqTest2/Extensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqTest2
+{
+    static class Extensions
+    {
+        public static IEnumerable<T> GetElementsByCondition<T>(this IEnumerable<T> collection,
+            Predicate<T> predicate) // linq.Where()
+        {
+            foreach (T item in collection)
+            {
+                if(predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        public static T GetFirstElementByCondition<T>(this IEnumerable<T> collection,
+            Predicate<T> predicate) // linq.FirstOrDefault
+        {
+            foreach (T item in collection)
+            {
+                if (predicate(item))
+                {
+                    return item;
+                }
+            }
+
+            return default(T);
+        }
+        public static void MakeAction<T>(this IEnumerable<T> collection, Action<T> action)
+        {
+            foreach (T item in collection)
+            {
+                action(item);
+            }
+        }
+    }
+}
---
+++ LinqTest2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+namespace LinqTest2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> x = new List<int>() { 3, 4, 2, 4, 6, 9, 2, 1, 3, 5, 21 };
+
+            IEnumerable<int> ts = x
+                .SkipWhile(a => a < 9)
+                .Where(a => a % 3 == 0);
+
+            //var list = x.Where(a => a % 3 == 0).ToList(); // correct
+
+            //var list2 = x.ToList().Where(a => a % 3 == 0); // Incorrect
+
+            foreach (int item in ts)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
